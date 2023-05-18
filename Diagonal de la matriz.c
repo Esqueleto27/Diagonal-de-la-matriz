0d5d87e72915a50b1e5af4a99c0e214cb28725fa
Esqueleto27 <matheofloresloor@gmail.com>
2023-05-18 12:15:06 -0500

    Crear la matriz

--- Diagonal de la matriz.c
+++ Diagonal de la matriz.c
@@ -1,1 +1,12 @@
 #include<stdio.h> 
+#include <stdlib.h>
+
+int main() {
+    int n, i, j;
+    
+    printf("Ingrese el tamaño de la matriz cuadrada: ");
+    scanf("%d", &n);
+    
+     int **matriz = (int **)malloc(n * sizeof(int *));
+    for (i = 0; i < n; i++) {
+        matriz[i] = (int *)malloc(n * sizeof(int));
